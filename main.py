51fc0d1ea077270589ba94e04e0da680fddba99d
Lenowo0 <lenowo727@gmail.com>
2022-11-11 23:14:53 +0300

    Clockin

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ from time import strftime
 
 root = Tk()
 root.title('Silly Clock')
+
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = Label(root, font=("DS-Digital", 70), background='black', foreground="pink")
+label.pack(anchor="center")
+
+time()
+mainloop()
+    
